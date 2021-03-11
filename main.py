c6a97e382a17c68e13bd20c93cd538582e588bf2
PlsDrop <60732228+PlsDrop@users.noreply.github.com>
2021-03-11 10:19:32 +0200

    Работает но неправильно
    Нужно как-то нули пихнуть в не рабочие дни

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print("Введите путь и/или имя файла:")
 
 path = input()
 
-countLines = 0
+checkFirstDate = True
 checkName = False
 
 f = open(path, 'r')
@@ -14,39 +14,63 @@ f2.write("Name / Date,")
 f2.close()
 
 for line in f:
-    countLines += 1
     name = line.split(',')[0]
     date = line.split(',')[1]
     time = line.split(',')[2]
     time = time.split('\n')[0]
     
-    f2 = open("result.csv", '+a')
-    f2.seek(0 - f2.tell(), 2)
+    f2 = open("result.csv", 'r')
 
     #ищем дату
-    if f2.readline().find(date) == -1:
-        f2.write(date + ",\n")
-    
+    firstLine = f2.readline()
+    firstLine = firstLine.split('\n')[0]
+    if firstLine.find(date) == -1:
+        nextLines = f2.read()
+        f2.close()
+
+        f2 = open("result.csv", 'w')
+        f2.write(firstLine + date + ",\n" + nextLines)
+        f2.close()
+
+  
     
     #ищем имя 
+    f2 = open("result.csv", 'r')
     for line in f2:
         if line.find(name) != -1:
-            f2.write(time + ',')
             checkName = True
+            
+            f2.seek(0)
+            firstPart = f2.read().split(line)[0]
+            f2.seek(0)
+            secondPart = f2.read().split(line)[1] 
+            f2.close()
+
+            line = line.split('\n')[0]
+
+            f2 = open("result.csv", 'w')
+            f2.write(firstPart + line + time + ',' + '\n' +  secondPart)
+            f2.close()
             break
+
+    
+
     
     if checkName == False:
-        f2.write('\n' + name + ',' + time + ',')
+        f2 = open("result.csv", 'a')
+        f2.write(name + ',' + time + ',\n')
+        f2.close()
     
-    f2.close()
-    
-    
-    
+   
     f2 = open("result.csv", 'r')
     print(f2.read())
     f2.close()
     input()
 
+    
+    
+
+
+    
+
 f.close()
-
-
